0fbd3cb589bb9450c484bd73a21fa8a319b516b6
Bernie Petters <bp4924@gmail.com>
2023-06-07 17:15:51 -0400

    initialize clean_text before loop

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import PyPDF2
 pdfreader = PyPDF2.PdfReader(open('story.pdf', 'rb'))
 speaker = pyttsx3.init()
 
+clean_text = ""
+print(f"pages {pdfreader.pages}")
 for page_num in range(len(pdfreader.pages)):
     text = pdfreader.pages[page_num].extract_text()
-    clean_text = text.strip().replace('/n', " ")
-    print(clean_text)
+    print(text)
+    clean_text += text.strip().replace('/n', '')
+
+print(clean_text)
 
 speaker.save_to_file(clean_text, 'story.mp3')
 speaker.runAndWait()
